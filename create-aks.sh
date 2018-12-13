58f5a6f26f033fcfb27560e02a8eb65e10b6942e
John Stallo <john.stallo@live.com>
2018-12-12 17:37:53 -0800

    Create AKS script

---
+++ create-aks.sh
@@ -0,0 +1,26 @@
+AKS_NAME="$1"
+if [ -z "$1" ]; then
+    AKS_NAME="bikesharing01"
+fi
+
+AKS_REGION="$2"
+if [ -z "$2" ]; then
+    AKS_REGION="eastus2"
+fi
+
+echo "===================================="
+echo "creating group: " $AKS_NAME
+az group create --name $AKS_NAME --location $AKS_REGION 
+
+echo "===================================="
+echo "creating AKS cluster: " $AKS_NAME
+az aks create -g $AKS_NAME -n $AKS_NAME --location $AKS_REGION --kubernetes-version 1.10.9 --enable-addons http_application_routing --node-vm-size Standard_DS2_v2 --node-count 3 --generate-ssh-keys --disable-rbac
+
+echo "===================================="
+echo "enabling Dev Spaces for AKS cluster: " $AKS_NAME
+az aks use-dev-spaces -g $AKS_NAME -n $AKS_NAME -s default -y
+
+echo "===================================="
+echo "Open ./bikesharingweb/azds.yaml, and change the apiHost line to point to your AKS cluster's zone name:"
+az aks show -g $AKS_NAME -n $AKS_NAME -o json --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName
+echo "===================================="
