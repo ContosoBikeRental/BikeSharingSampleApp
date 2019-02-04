64a44a62c51ef30ae891b78c0721a6f512dcb7fa
John Stallo <john.stallo@live.com>
2019-02-04 15:18:02 -0800

    Reduce cluster size + other tweaks

--- create-aks.sh
+++ create-aks.sh
@@ -8,13 +8,18 @@ if [ -z "$2" ]; then
     AKS_REGION="eastus2"
 fi
 
+K8S_VERSION="$3"
+if [ -z "$3" ]; then
+    K8S_VERSION="1.11.6"
+fi
+
 echo "===================================="
 echo "creating group: " $AKS_NAME
 az group create --name $AKS_NAME --location $AKS_REGION 
 
 echo "===================================="
 echo "creating AKS cluster: " $AKS_NAME
-az aks create -g $AKS_NAME -n $AKS_NAME --location $AKS_REGION --kubernetes-version 1.12.4 --enable-addons http_application_routing --node-vm-size Standard_DS2_v2 --node-count 3 --generate-ssh-keys --disable-rbac
+az aks create -g $AKS_NAME -n $AKS_NAME --location $AKS_REGION --kubernetes-version $K8S_VERSION --enable-addons http_application_routing --node-vm-size Standard_DS2_v2 --node-count 1 --generate-ssh-keys --disable-rbac
 
 echo "===================================="
 echo "enabling Dev Spaces for AKS cluster: " $AKS_NAME
